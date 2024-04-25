25ad780742958df7eef202a1fa6cb5d0e5e35f74
Matheus Penchel <matheus.penchel@gmail.com>
2024-04-25 05:55:28 -0300

    Finished the challenge in the most simple and ugly way

--- playground.rb
+++ playground.rb
@@ -2,23 +2,68 @@ kill_expression = /^\s*\d+:\d{2}\sKill/
 new_match_expression = /^\s*\d+:\d{2}\sInitGame/
 user_connected_expression = /^\s*\d+:\d{2}\sClientUserinfoChanged/
 get_username_expression = /^\s*\d+:\d{2}\sClientUserinfoChanged:\s\d+\s*n\\+(?<username>[^\\]+)/
+kill_group_expression = /Kill:[\d\s]+:\s*(?<killer>.+) killed (?<victim>.+) by (?<death_cause>.+)/
 
-kill_count = 0
 match_count = 0
-players = []
+match_hash  = {}
 
 File.open('qgames.log').each do |line|
   puts line
 
-  match_count += 1 if line.match?(new_match_expression)
-  kill_count += 1 if line.match?(kill_expression)
+  # Starting new match
+  if line.match?(new_match_expression)
+    match_count += 1
 
-  players << line.match(get_username_expression)[:username] if line.match?(user_connected_expression)
+    match_hash["game_#{match_count}"] = {
+      "total_kills" => 0,
+      "players" => [],
+      "kills" => {},
+      "kills_by_means" => {}
+    }
+  end
+
+  # Player connected
+  if line.match?(user_connected_expression)
+    username = line.match(get_username_expression)[:username]
+
+    unless match_hash["game_#{match_count}"]["players"].include?(username)
+      match_hash["game_#{match_count}"]["players"] << username
+      match_hash["game_#{match_count}"]["kills"][username] = 0
+    end
+  end
+
+  # Killing Spree
+  if line.match?(kill_expression)
+    match_hash["game_#{match_count}"]["total_kills"] += 1
+
+    kill_data = line.match(kill_group_expression)
+
+    if kill_data[:killer] == '<world>'
+      match_hash["game_#{match_count}"]["kills"][kill_data[:victim]] -= 1
+    else
+      match_hash["game_#{match_count}"]["kills"][kill_data[:killer]] += 1
+    end
+
+    match_hash["game_#{match_count}"]["kills_by_means"][kill_data[:death_cause]] ||= 0
+    match_hash["game_#{match_count}"]["kills_by_means"][kill_data[:death_cause]] += 1
+  end
+
 end
 
-puts "\n\n Total kill count: #{kill_count}"
-puts "\n Number of matches: #{match_count}"
-puts "\n Players: #{players.uniq.join(', ')}"
+match_hash.each do |key, value|
+  puts '####################################'
+  puts key
+  puts "Total Kills: #{value['total_kills']}"
+  puts "Players: #{value['players'].join(', ')}\n"
+  puts "Kills:\n"
+  value['kills'].each do |username, kill_count|
+    puts "    #{username}: #{kill_count}\n"
+  end
+  puts "Death causes:\n"
+  value['kills_by_means'].each do |death_cause, kill_count|
+    puts "    #{death_cause}: #{kill_count}\n"
+  end
+end
 
 # Desired output per match:
 #
